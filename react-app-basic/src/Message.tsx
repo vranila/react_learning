856dbf4f1ad72935f5397ecd63f4f84068c0ce71
vranila <acroiris123@gmail.com>
2026-03-02 12:24:05 +0530

    Initial React Vite project

---
+++ react-app-basic/src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing --- capitalize first letter of the every word
+function Message()
+{
+    const name='Anila';
+    //JSX:Javascript  XML
+    if(name)
+        return <h1>Hello By {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
